a517f63b53f60e6f337ea768fe62cb2726137132
damipic100-sys <damipic100@gmail.com>
2025-12-27 10:24:19 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+const app = document.getElementById("app");
+
+app.innerHTML = `
+  <table>
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Jugador</th>
+        <th>Rank</th>
+        <th>LP</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Ejemplo#LAS</td>
+        <td>Diamond I</td>
+        <td>75</td>
+      </tr>
+    </tbody>
+  </table>
+`;
