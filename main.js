5839ea92a076e9da936587e08325246d9c4f0ae6
damipic100-sys <damipic100@gmail.com>
2025-12-28 11:35:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 const app = document.getElementById("app");
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
+const supabase = createClient(
+  "https://klnnytucxqfojusrkaya.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtsbm55dHVjeHFmb2p1c3JrYXlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY4MDEwMjksImV4cCI6MjA4MjM3NzAyOX0.HFCHI16jLS6omtZrAfC-BGhzb0UEA8NVTioTF7-rzQk"
+);
+async function cargarLadder() {
+  const { data, error } = await supabase
+    .from("players")
+    .select("*")
+    .order("elo", { ascending: false });
+
+  if (error) {
+    app.innerHTML = "Error cargando ladder";
+    return;
+  }
 app.innerHTML = `
   <table>
     <thead>
@@ -20,3 +35,4 @@ app.innerHTML = `
     </tbody>
   </table>
 `;
+cargarLadder();
