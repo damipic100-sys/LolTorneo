dd7cfc5c10cdb5dcd30b0512953861be1709cdbe
damipic100-sys <damipic100@gmail.com>
2025-12-28 12:30:56 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const app = document.getElementById("app");
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
 const supabase = createClient(
   "https://klnnytucxqfojusrkaya.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtsbm55dHVjeHFmb2p1c3JrYXlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY4MDEwMjksImV4cCI6MjA4MjM3NzAyOX0.HFCHI16jLS6omtZrAfC-BGhzb0UEA8NVTioTF7-rzQk"
 );
+const app = document.getElementById("app");
+app.textContent = "Cargando ladder...";
+
 async function cargarLadder() {
   const { data, error } = await supabase
     .from("players")
@@ -21,24 +23,20 @@ async function cargarLadder() {
     app.textContent = "No hay jugadores en la ladder";
     return;
   }
-app.innerHTML = `
-  <table>
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>Jugador</th>
-        <th>Rank</th>
-        <th>LP</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>1</td>
-        <td>Ejemplo#LAS</td>
-        <td>Diamond I</td>
-        <td>75</td>
-      </tr>
-    </tbody>
-  </table>
-`;
+
+  app.innerHTML = `
+    <table>
+      <tr><th>#</th><th>Jugador</th><th>Rank</th><th>LP</th></tr>
+      ${data.map((p,i)=>`
+        <tr>
+          <td>${i+1}</td>
+          <td>${p.riot_id}</td>
+          <td>${p.rank}</td>
+          <td>${p.lp}</td>
+        </tr>
+      `).join("")}
+    </table>
+  `;
+}
+
 cargarLadder();
